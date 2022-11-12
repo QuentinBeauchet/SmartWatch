f853f3e13599aea5dab8f5a9990efbe78b442874
QuentinBeauchet <quentinbeauchet@free.fr>
2022-11-12 16:28:43 +0100

    Init dev env

---
+++ Application/app/src/main/java/com/example/application/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.application
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.application.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
---
+++ server/schema.sql
@@ -0,0 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db
+-- Généré le : sam. 12 nov. 2022 à 14:47
+-- Version du serveur : 10.9.4-MariaDB-1:10.9.4+maria~ubu2204
+-- Version de PHP : 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `watch`
+--
+
+-- --------------------------------------------------------
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
